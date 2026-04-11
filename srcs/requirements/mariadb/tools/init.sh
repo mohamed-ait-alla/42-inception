a734398d3a937684f7318116843103c83cdb3686
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-04-11 04:24:24 -0400

    fix(mariadb): fix a mysql unrecoginized service issue

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-service mysql start
+# Starting mysql service
+mysqld_safe &
 
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+# Initializing Database
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p $MYSQL_ROOT_PASSWORD shutdown
+
+wait
 
 exec mysqld_safe
