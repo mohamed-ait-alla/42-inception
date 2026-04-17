a329e1c8256651b095b4fcbdb9348085c48cd384
Mohamed-Ait-Alla <mohamedmhdo2000@gmail.com>
2026-04-17 15:09:18 -0400

    fix(bonus/redis): fix a not writeable to file system issue in redis-cache by adding www-data to to ftp user group

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# initializing the security staging area used by vsftpd for privilege dropping process
 mkdir -p /var/run/vsftpd/empty
 chmod 755 /var/run/vsftpd/empty
 
 
+# setting up ftp user
 useradd -m -d /var/www/html $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-chown -R $FTP_USER:$FTP_USER /var/www/html
+usermod -aG www-data $FTP_USER
 
+# start vsftpd in foreground
 exec vsftpd /etc/vsftpd.conf
